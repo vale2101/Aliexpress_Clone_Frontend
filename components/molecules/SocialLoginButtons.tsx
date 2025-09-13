601b7df00253c1c0c3b80f0c86f9ad3b69184b46
vale2101 <valeria.herrerap@autonoma.edu.co>
2025-09-12 19:36:59 -0500

    Errores Vercel

--- components/molecules/SocialLoginButtons.tsx
+++ components/molecules/SocialLoginButtons.tsx
@@ -23,6 +23,7 @@ const SocialLoginButtons: React.FC = () => {
   return (
     <div className="space-y-3">
       <SocialButton
+        provider="Google"
         onClick={handleGoogleLogin}
         icon={
           <svg viewBox="0 0 24 24">
@@ -32,27 +33,26 @@ const SocialLoginButtons: React.FC = () => {
             <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
           </svg>
         }
-        text="Google"
       />
 
       <SocialButton
+        provider="Facebook"
         onClick={handleFacebookLogin}
         icon={
           <svg fill="#1877F2" viewBox="0 0 24 24">
             <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
           </svg>
         }
-        text="Facebook"
       />
 
       <SocialButton
+        provider="Apple"
         onClick={handleAppleLogin}
         icon={
           <svg fill="#000000" viewBox="0 0 24 24">
             <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
           </svg>
         }
-        text="Apple"
       />
 
       {/* Más opciones / Twitter */}
@@ -75,13 +75,13 @@ const SocialLoginButtons: React.FC = () => {
           </button>
         ) : (
           <SocialButton
+            provider="Twitter"
             onClick={handleTwitterLogin}
             icon={
-              <svg fill="#000000" viewBox="0 0 24 24">
+              <svg fill="#1DA1F2" viewBox="0 0 24 24">
                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
               </svg>
             }
-            text="Twitter"
           />
         )}
       </div>
